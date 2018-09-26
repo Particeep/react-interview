fbcb557aae68ca2ead85d724f47c46d2f3066df9
Alexandre Annic <alexandre.annic1@gmail.com>
2018-09-26 16:33:34 +0200

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,29 @@
+const movies = [
+  {
+    id: '1',
+    title: 'Oceans 8',
+    category: 'Comedy',
+    likes: 4,
+    dislikes: 1
+  }, {
+    id: '2',
+    title: 'Midnight Sun',
+    category: 'Comedy',
+    likes: 2,
+    dislikes: 0
+  }, {
+    id: '3',
+    title: 'Les indestructibles 2',
+    category: 'Animation',
+    likes: 3,
+    dislikes: 1
+  }, {
+    id: '4',
+    title: 'Sans un bruit',
+    category: 'Thriller',
+    likes: 6,
+    dislikes: 6
+  },
+];
+
+export const movies$ = new Promise((resolve, reject) => setTimeout(resolve, 100, movies));
