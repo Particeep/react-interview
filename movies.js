2af7daf152ffd6a6becf526b83e7c41fc1a14f6f
Alexandre Annic <alexandre.annic1@gmail.com>
2018-10-05 10:34:24 +0200

    Update movies.js

--- movies.js
+++ movies.js
@@ -23,7 +23,43 @@ const movies = [
     category: 'Thriller',
     likes: 6,
     dislikes: 6
+  }, {
+    id: '5',
+    title: 'Creed II',
+    category: 'Drame',
+    likes: 16,
+    dislikes: 2
+  }, {
+    id: '6',
+    title: 'Pulp Fiction',
+    category: 'Thriller',
+    likes: 11,
+    dislikes: 3
+  }, {
+    id: '7',
+    title: 'Pulp Fiction',
+    category: 'Thriller',
+    likes: 12333,
+    dislikes: 32
+  }, {
+    id: '8',
+    title: 'Seven',
+    category: 'Thriller',
+    likes: 2,
+    dislikes: 1
+  }, {
+    id: '9',
+    title: 'Inception',
+    category: 'Thriller',
+    likes: 2,
+    dislikes: 1
+  }, {
+    id: '10',
+    title: 'Gone Girl',
+    category: 'Thriller',
+    likes: 22,
+    dislikes: 12
   },
-];
+]
 
-export const movies$ = new Promise((resolve, reject) => setTimeout(resolve, 100, movies));
+export const movies$ = new Promise((resolve, reject) => setTimeout(resolve, 100, movies))
