ac5fbe7322c3295e8210151742b5292358f4b5fe
Nelson Zamith <zamith.nelson@gmail.com>
2018-11-14 13:03:56 +0100

    defining a css grid with flexbox

--- src/App.js
+++ src/App.js
@@ -1,74 +1,88 @@
 import React from 'react'
 
-const movies = [
-  {
-    id: '1',
-    title: 'Oceans 8',
-    category: 'Comedy',
-    likes: 4,
-    dislikes: 1
-  }, {
-    id: '2',
-    title: 'Midnight Sun',
-    category: 'Comedy',
-    likes: 2,
-    dislikes: 0
-  }, {
-    id: '3',
-    title: 'Les indestructibles 2',
-    category: 'Animation',
-    likes: 3,
-    dislikes: 1
-  }, {
-    id: '4',
-    title: 'Sans un bruit',
-    category: 'Thriller',
-    likes: 6,
-    dislikes: 6
-  }, {
-    id: '5',
-    title: 'Creed II',
-    category: 'Drame',
-    likes: 16,
-    dislikes: 2
-  }, {
-    id: '6',
-    title: 'Pulp Fiction',
-    category: 'Thriller',
-    likes: 11,
-    dislikes: 3
-  }, {
-    id: '7',
-    title: 'Pulp Fiction',
-    category: 'Thriller',
-    likes: 12333,
-    dislikes: 32
-  }, {
-    id: '8',
-    title: 'Seven',
-    category: 'Thriller',
-    likes: 2,
-    dislikes: 1
-  }, {
-    id: '9',
-    title: 'Inception',
-    category: 'Thriller',
-    likes: 2,
-    dislikes: 1
-  }, {
-    id: '10',
-    title: 'Gone Girl',
-    category: 'Thriller',
-    likes: 22,
-    dislikes: 12
-  },
-]
-
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: [
+        {
+          id: '1',
+          title: 'Oceans 8',
+          category: 'Comedy',
+          likes: 4,
+          dislikes: 1
+        }, {
+          id: '2',
+          title: 'Midnight Sun',
+          category: 'Comedy',
+          likes: 2,
+          dislikes: 0
+        }, {
+          id: '3',
+          title: 'Les indestructibles 2',
+          category: 'Animation',
+          likes: 3,
+          dislikes: 1
+        }, {
+          id: '4',
+          title: 'Sans un bruit',
+          category: 'Thriller',
+          likes: 6,
+          dislikes: 6
+        }, {
+          id: '5',
+          title: 'Creed II',
+          category: 'Drame',
+          likes: 16,
+          dislikes: 2
+        }, {
+          id: '6',
+          title: 'Pulp Fiction',
+          category: 'Thriller',
+          likes: 11,
+          dislikes: 3
+        }, {
+          id: '7',
+          title: 'Pulp Fiction',
+          category: 'Thriller',
+          likes: 12333,
+          dislikes: 32
+        }, {
+          id: '8',
+          title: 'Seven',
+          category: 'Thriller',
+          likes: 2,
+          dislikes: 1
+        }, {
+          id: '9',
+          title: 'Inception',
+          category: 'Thriller',
+          likes: 2,
+          dislikes: 1
+        }, {
+          id: '10',
+          title: 'Gone Girl',
+          category: 'Thriller',
+          likes: 22,
+          dislikes: 12
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
+        <div className="movie-list">
+          {
+            this.state.movies.map((movie) => (
+              <div className="movie-card">
+                <p className="movie-title">{movie.title}</p>
+                <p className="movie-category">Catégorie: {movie.category}</p>
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
