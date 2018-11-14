0e84fa6c9c9ddc799d43d5eff996c4950f558783
Nelson Zamith <zamith.nelson@gmail.com>
2018-11-15 00:04:23 +0100

    implementing the category filter

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ class App extends React.Component {
     super()
     this.state = {
       movies: [],
-      moviesPerPage: 20
+      moviesPerPage: 20,
+      currentCategory: 'all'
     }
+    this.setCategory = this.setCategory.bind(this)
   }
 
   componentDidMount() {
@@ -19,45 +21,76 @@ class App extends React.Component {
       })
   }
 
+  setCategory(category) {
+    this.setState({
+      currentCategory: category
+    })
+  }
+
+  getAvailableCategories() {
+    let categories = []
+    this.state.movies.forEach((movie) => {
+      if (!categories.includes(movie.category))
+        categories.push(movie.category)
+    })
+    return categories
+  }
+
+  getMovies(category, moviesPerPage) {
+    let movies = this.state.movies.slice()
+    if (category != 'all') {
+      movies.forEach((movie, index) => {
+        if (movie.category !== category)
+          movies.splice(index, 1)
+      })
+    }
+    return (
+      movies.map((movie) => (
+        <div className="movie-card" key={movie.id}>
+          <p className="movie-title">{movie.title}</p>
+          <p className="movie-category">Catégorie: {movie.category}</p>
+          <div className="like-bar">
+            <div className="like-button">
+              <i className="far fa-thumbs-up"></i> {movie.likes}
+            </div>
+            <div className="dislike-button">
+              <i className="far fa-thumbs-down"></i> {movie.dislikes}
+            </div>
+          </div>
+          <div className="delete-movie">
+            <i className="far fa-window-close"></i>
+          </div>
+        </div>
+      ))
+    )
+  }
+
   render() {
     return (
       <div>
         <div className="movie-filters">
           <div className="filter">
             Catégorie :
-            <select>
-              <option>Toutes</option>
+            <select onChange={(e) => this.setCategory(e.target.value)}>
+              <option value="all">Toutes</option>
+              {
+                this.getAvailableCategories().map((category, index) => (
+                  <option value={category} key={index}>{category}</option>
+                ))
+              }
             </select>
           </div>
           <div className="filter">
             Films par page :
             <select defaultValue="12">
-              <option>4</option>
-              <option>8</option>
-              <option>12</option>
+              <option value="4">4</option>
+              <option value="8">8</option>
+              <option value="12">12</option>
             </select>
           </div>
         </div>
         <div className="movie-list">
-          {
-            this.state.movies.map((movie) => (
-              <div className="movie-card" key={movie.id}>
-                <p className="movie-title">{movie.title}</p>
-                <p className="movie-category">Catégorie: {movie.category}</p>
-                <div className="like-bar">
-                  <div className="like-button">
-                    <i className="far fa-thumbs-up"></i> {movie.likes}
-                  </div>
-                  <div className="dislike-button">
-                    <i className="far fa-thumbs-down"></i> {movie.dislikes}
-                  </div>
-                </div>
-                <div className="delete-movie">
-                  <i className="far fa-window-close"></i>
-                </div>
-              </div>
-            ))
-          }
+          {this.getMovies(this.state.currentCategory, 12)}
         </div>
       </div>
     );
