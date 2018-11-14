7f039b58a15dc6127eff53f03d460c9cb4c632d9
Nelson Zamith <zamith.nelson@gmail.com>
2018-11-14 20:23:44 +0100

    fetching movies via XHR
    and providing movies list as JSON file
    in public/

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
 import React from 'react'
+import axios from "axios"
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      movies: [
-        {
-          id: '1',
-          title: 'Oceans 8',
-          category: 'Comedy',
-          likes: 4,
-          dislikes: 1
-        }, {
-          id: '2',
-          title: 'Midnight Sun',
-          category: 'Comedy',
-          likes: 2,
-          dislikes: 0
-        }, {
-          id: '3',
-          title: 'Les indestructibles 2',
-          category: 'Animation',
-          likes: 3,
-          dislikes: 1
-        }, {
-          id: '4',
-          title: 'Sans un bruit',
-          category: 'Thriller',
-          likes: 6,
-          dislikes: 6
-        }, {
-          id: '5',
-          title: 'Creed II',
-          category: 'Drame',
-          likes: 16,
-          dislikes: 2
-        }, {
-          id: '6',
-          title: 'Pulp Fiction',
-          category: 'Thriller',
-          likes: 11,
-          dislikes: 3
-        }, {
-          id: '7',
-          title: 'Pulp Fiction',
-          category: 'Thriller',
-          likes: 12333,
-          dislikes: 32
-        }, {
-          id: '8',
-          title: 'Seven',
-          category: 'Thriller',
-          likes: 2,
-          dislikes: 1
-        }, {
-          id: '9',
-          title: 'Inception',
-          category: 'Thriller',
-          likes: 2,
-          dislikes: 1
-        }, {
-          id: '10',
-          title: 'Gone Girl',
-          category: 'Thriller',
-          likes: 22,
-          dislikes: 12
-        },
-      ]
+      movies: [],
+      moviesPerPage: 20
     }
+  }
+
+  componentDidMount() {
+    axios.get('movies.json')
+      .then((response) => {
+        this.setState({
+          movies: response.data
+        });
+      })
   }
 
   render() {
@@ -82,17 +31,17 @@ class App extends React.Component {
           </div>
           <div className="filter">
             Films par page :
-            <select>
+            <select defaultValue="12">
               <option>4</option>
               <option>8</option>
-              <option selected>12</option>
+              <option>12</option>
             </select>
           </div>
         </div>
         <div className="movie-list">
           {
             this.state.movies.map((movie) => (
-              <div className="movie-card">
+              <div className="movie-card" key={movie.id}>
                 <p className="movie-title">{movie.title}</p>
                 <p className="movie-category">Catégorie: {movie.category}</p>
                 <div className="like-bar">
