e53a20bdbb2d35a2e4e0f130d76418e03dc3eb56
samirkhennache <samirkhennache@gmail.com>
2018-11-30 12:14:00 +0100

    clean the code

--- list-film/src/components/FilmCard.js
+++ list-film/src/components/FilmCard.js
@@ -1,30 +1,47 @@
-import React, { Component } from 'react'
-
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class FilmCard extends Component {
-    state = {
-        isLiked :false
-     }
-     handelLike(film){
-        const {isLiked} = this.state
-        this.setState({isLiked:!isLiked},()=>this.props.handelLike(film,this.state.isLiked))
+  state = {
+    isLiked: false
+  };
 
-     }
-    render() {
-        const {isLiked} = this.state
-        const {film} = this.props;
-        const {title, category,likes,dislikes} = film;
-        return (
-            <div>
-             <h1>{title}</h1>
-             <p>{category}</p>
-             <p>like :{likes} dislike:{dislikes}</p>
-             <button onClick={() =>this.props.handelDelete(film)}>Delete</button>
-             <button onClick ={() =>this.handelLike(film) } >{!isLiked? "Like": "Dislike"}</button>
-            </div>
-         );
-    }
+  handelLike(film) {
+    const { handelLike } = this.props;
+    const { isLiked } = this.state;
+    this.setState({ isLiked: !isLiked }, () => {
+      // const { isNewLiked } = this.state;
+      handelLike(film, this.state.isLiked);
+    });
+  }
+
+  render() {
+    const { isLiked } = this.state;
+    const { film, handelDelete } = this.props;
+    const { title, category, likes, dislikes } = film;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{category}</p>
+        <p>
+          like :{likes} dislike:{dislikes}
+        </p>
+        <button type="button" onClick={() => handelDelete(film)}>
+          Delete
+        </button>
+        <button type="button" onClick={() => this.handelLike(film)}>
+          {!isLiked ? "Like" : "Dislike"}
+        </button>
+      </div>
+    );
+  }
 }
+
+FilmCard.propTypes = {
+  handelLike: PropTypes.func.isRequired,
+  handelDelete: PropTypes.func.isRequired
+};
+
+// FilmCard.defaultProps = { handelLike: "toto" };
 
 export default FilmCard;
--- list-film/src/components/ListFilm.js
+++ list-film/src/components/ListFilm.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import FilmCard from './FilmCard';
-import {movies$} from '../movies'
-import   './listFilm.css'
-import { ClipLoader } from 'react-spinners';
-import Select from 'react-select';
-
+import React, { Component } from "react";
+import { ClipLoader } from "react-spinners";
+import Select from "react-select";
+import FilmCard from "./FilmCard";
+import { movies$ } from "../movies";
+import "./listFilm.css";
 
 const options = [
   { value: 4, label: 4 },
@@ -13,213 +12,263 @@ const options = [
 ];
 
 class ListFilm extends Component {
-    state = {
-				filmList : [],
-				categoriesObject :[{}],
-				selectedCategories :[],
-        loading :false,
-        currentNumberPage: 1,
-        filmsPerPage: [],
-        nbFilmPerPage :4,
-        numberResultStart: 0,
-				numberResultEnd: 0,
-     }
-    componentDidMount = () => {
-        movies$.then(data => {this.setState({filmList :data,loading:true},()=>this.getMoviesPerPage())
-        });
+  state = {
+    filmList: [],
+    categoriesObject: [{}],
+    selectedCategories: [],
+    loading: false,
+    currentNumberPage: 1,
+    filmsPerPage: [],
+    nbFilmPerPage: 4
+  };
 
-    }
-    handelDelete =(film) =>{
+  componentDidMount = () => {
+    movies$.then(data => {
+      this.setState({ filmList: data, loading: true }, () =>
+        this.getMoviesPerPage()
+      );
+    });
+  };
 
-        this.setState(previousState => {
+  handelDelete = film => {
+    this.setState(
+      previousState => ({
+        filmList: [...previousState.filmList.filter(e => e.id !== film.id)]
+      }),
+      () => this.getMoviesPerPage()
+    );
+  };
+
+  handelLike = (film, stateChild) => {
+    // const { isLiked } = stateChild;
+    if (stateChild) {
+      // film.likes += 1;
+      console.log("one like", film.likes);
+
+      const { likes, dislikes } = film;
+
+      this.setState(previousState => ({
+        filmList: previousState.filmList.map(e => {
+          if (e.id === film.id)
             return {
-						filmList:[...previousState.filmList.filter(e => e.id !== film.id)],
+              category: film.category,
+              title: film.title,
+              id: film.id,
+              likes: likes + 1,
+              dislikes
             };
-				},()=>this.getMoviesPerPage());
-
+          return e;
+        })
+      }));
+      // this.setState(previousState => ({
+      //   filmList: [
+      //     ...previousState.filmList.filter(e => e.id !== film.id),
+      //     { likes: [likes + 1], dislikes }
+      //   ]
+      // }));
+    } else {
+      const { likes, dislikes } = film;
+      console.log(likes);
+      this.setState(previousState => ({
+        filmList: previousState.filmList.map(movie => {
+          if (movie.id === film.id)
+            return {
+              category: film.category,
+              title: film.title,
+              id: film.id,
+              likes,
+              dislikes: dislikes + 1
+            };
+          return movie;
+        })
+      }));
+      // this.setState(previousState => ({
+      //   filmList: [
+      //     ...previousState.filmList.filter(e => e.id !== film.id),
+      //     { likes, dislikes: dislikes + 1 }
+      //   ]
+      // }));
     }
-    handelLike = (film,isLiked) =>{
-        if(isLiked){
-            film.likes +=1;
+  };
 
-        console.log(film);
-
-            this.setState(previousState => {
-                return {
-                filmList:
-                previousState.filmList.map(e => {
-                    if(e.id === film.id)
-                        return film
-                    return e
-                })
-                };
-            })
-        }
-        else{
-            film.dislikes +=1;
-            this.setState(previousState => {
-                return {
-                filmList:
-                previousState.filmList.map(e => {
-                    if(e.id === film.id)
-                        return film
-                    return e
-                })
-                };
-            })
-        }
+  getMoviesPerPage = () => {
+    //	this.getSelectdCategories()
+    const { currentNumberPage, nbFilmPerPage, filmList } = this.state;
+    if (this.filmFilter().length !== 0) {
+      const numberResultStart = (currentNumberPage - 1) * nbFilmPerPage;
+      let numberResultEnd = currentNumberPage * nbFilmPerPage;
+      if (numberResultEnd > this.filmFilter().length) {
+        numberResultEnd = this.filmFilter().length;
+      }
+      const pageArray = this.filmFilter().slice(
+        numberResultStart,
+        numberResultEnd
+      );
+      console.log("mes films ", pageArray);
+      this.setState(
+        {
+          filmsPerPage: pageArray
+        },
+        () => this.getCategories()
+      );
+    } else {
+      const numberResultStart = (currentNumberPage - 1) * nbFilmPerPage;
+      let numberResultEnd = currentNumberPage * nbFilmPerPage;
+      if (numberResultEnd > filmList.length) {
+        numberResultEnd = filmList.length;
+      }
+      const pageArray = filmList.slice(numberResultStart, numberResultEnd);
+      console.log("mes films ", pageArray);
+      this.setState(
+        {
+          filmsPerPage: pageArray
+        },
+        () => this.getCategories()
+      );
     }
-    getMoviesPerPage = () => {
-		//	this.getSelectdCategories()
-				const { currentNumberPage,nbFilmPerPage,filmList } = this.state;
-        const numberResultStart =
-						(currentNumberPage - 1) * nbFilmPerPage;
-        let numberResultEnd = currentNumberPage * nbFilmPerPage;
-        if (numberResultEnd > this.filmFilter().length) {
-            numberResultEnd = this.filmFilter().length;
+  };
+
+  getSelectdCategories = () => {
+    const { filmList, selectedCategories } = this.state;
+    const categories = [];
+    let newSelectedCategories = [];
+    const myCategories = filmList
+      .map(film => film.category)
+      .filter(category => !categories.includes(category));
+    newSelectedCategories = selectedCategories.filter(category =>
+      myCategories.includes(category.value)
+    );
+    this.setState({ selectedCategories: newSelectedCategories });
+  };
+
+  getCategories = () => {
+    this.getSelectdCategories();
+    const { filmList } = this.state;
+    const selectedCategories = [];
+    const categoriesObject = [{}];
+    filmList
+      .map(film => film.category)
+      .filter(category => {
+        if (!selectedCategories.includes(category)) {
+          selectedCategories.push(category);
+          categoriesObject.push({ value: category, label: category });
         }
-				const pageArray = this.filmFilter().slice(numberResultStart, numberResultEnd);
-				console.log("mes films ",pageArray);
-				if(pageArray.length ===0){
-					this.setState({currentNumberPage:1})
-				}
-        this.setState({
-            filmsPerPage: pageArray,
-            numberResultStart: numberResultStart,
-            numberResultEnd: numberResultEnd,
+        return category;
+      });
+    this.setState({ categoriesObject });
+  };
 
-				},()=>this.getCategories());
-		};
-		getSelectdCategories =() =>{
-			const {filmList,selectedCategories} = this.state;
-			 const categories =[]
-			let newSelectedCategories =[]
-			const myCategories = filmList.map(film =>film.category).filter(category =>(!categories.includes(category)))
-			 console.log("nouvelle categories",categories);
-			 newSelectedCategories= selectedCategories.filter(category =>myCategories.includes(category.value))
-			 this.setState({selectedCategories:newSelectedCategories})
-		}
-		getCategories =() =>{
-			this.getSelectdCategories()
-		  const {filmList} = this.state;
-			const selectedCategories =[];
-			const categoriesObject =[{}];
-			filmList.map(film =>film.category).filter(category =>{
-				if(!selectedCategories.includes(category)){
-					selectedCategories.push(category)
-					categoriesObject.push({value:category,label:category})
-				}
-			})
-			this.setState({categoriesObject})
-		}
-		filmFilter = () =>{
-		const {filmList,selectedCategories} = this.state;
-		console.log("filmList",filmList);
-		console.log("selectedCategories",selectedCategories);
-
-		const options = selectedCategories.map(option => option.value)
-			console.log("mes catégories", options);
-
-	if(options.length!==0){
-		return filmList.filter(film =>options.includes(film.category) )
-	}
-
-	return filmList
-
-		}
-    handlerButtonPrevious = () => {
-        const { currentNumberPage } = this.state;
-        if (currentNumberPage > 1) {
-            this.setState(
-            {
-                currentNumberPage: currentNumberPage - 1,
-                filmsPerPage: []
-            },() =>this.getMoviesPerPage()
-            );
-        }
-    };
-
-		/**
-     * Go to the next page
-     */
-    handlerButtonNext = () => {
-        const { currentNumberPage,nbFilmPerPage} = this.state;
-        if (this.filmFilter().length / nbFilmPerPage > currentNumberPage) {
-            this.setState(
-            {
-                currentNumberPage: currentNumberPage + 1,
-                filmsPerPage: []
-            },() =>this.getMoviesPerPage()
-            );
-        }
-		};
-		handleChange = (selectedOption) => {
-			this.setState({ nbFilmPerPage:selectedOption.value,currentNumberPage:1 },() =>this.getMoviesPerPage());
-		}
-
-		handleChangeCategory = (selectedOption) =>{
-			console.log(selectedOption);
-
-			this.setState({ selectedCategories:selectedOption},() =>this.getMoviesPerPage());
-		}
-    render() {
-			const {
-				loading,
-				filmList,
-				categoriesObject,
-				selectedCategories,
-				currentNumberPage,
-				filmsPerPage,
-				nbFilmPerPage,
-			} = this.state;
-
-
-				if(loading){
-					if (filmList.length !== 0) {
-        return (
-					<div>
-            <div className = "ListFilm">
-								 {filmsPerPage.map(film =>
-								 <div key={film.id} className ="FilmCard">
-								 <FilmCard
-								 key={film.id}
-								 film = {film}
-								 handelDelete ={this.handelDelete}
-								 handelLike={this.handelLike} />
-								 </div>)}
-						</div>
-						<button onClick={this.handlerButtonPrevious}>{"\u003C"}</button>
-						{currentNumberPage}
-						<button onClick={this.handlerButtonNext}>{"\u003E"}</button>
-						<Select
-							name ="nbFilmPerPage"
-							value={nbFilmPerPage}
-							onChange={this.handleChange}
-							options={options}
-						/>
-							<Select
-							// value ={categoriesObject}
-							value={selectedCategories}
-							onChange={this.handleChangeCategory}
-							options={categoriesObject}
-							isMulti ={true}
-						/>
-					</div>
-				 );
-				}
-				}else{
-					return (
-						<div className='sweet-loading'>
-								<ClipLoader
-								sizeUnit={"px"}
-								size={150}
-								color={'#123abc'}
-								loading={loading}
-								/>
-						</div>
-				)
-				}
+  filmFilter = () => {
+    const { filmList, selectedCategories } = this.state;
+    const myOptions = selectedCategories.map(option => option.value);
+    if (selectedCategories.length !== 0) {
+      return filmList.filter(film => myOptions.includes(film.category));
     }
+
+    return filmList;
+  };
+
+  handlerButtonPrevious = () => {
+    const { currentNumberPage } = this.state;
+    if (currentNumberPage > 1) {
+      this.setState(
+        {
+          currentNumberPage: currentNumberPage - 1,
+          filmsPerPage: []
+        },
+        () => this.getMoviesPerPage()
+      );
+    }
+  };
+
+  /**
+   * Go to the next page
+   */
+  handlerButtonNext = () => {
+    const { currentNumberPage, nbFilmPerPage } = this.state;
+    if (this.filmFilter().length / nbFilmPerPage > currentNumberPage) {
+      this.setState(
+        {
+          currentNumberPage: currentNumberPage + 1,
+          filmsPerPage: []
+        },
+        () => this.getMoviesPerPage()
+      );
+    }
+  };
+
+  handleChange = selectedOption => {
+    this.setState(
+      { nbFilmPerPage: selectedOption.value, currentNumberPage: 1 },
+      () => this.getMoviesPerPage()
+    );
+  };
+
+  handleChangeCategory = selectedOption => {
+    this.setState({ selectedCategories: selectedOption }, () =>
+      this.getMoviesPerPage()
+    );
+  };
+
+  render() {
+    const {
+      loading,
+      filmList,
+      categoriesObject,
+      selectedCategories,
+      currentNumberPage,
+      filmsPerPage,
+      nbFilmPerPage
+    } = this.state;
+
+    if (loading && filmList.length !== 0) {
+      return (
+        <div>
+          <div className="ListFilm">
+            {filmsPerPage.map(film => (
+              <div key={film.id} className="FilmCard">
+                <FilmCard
+                  key={film.id}
+                  film={film}
+                  handelDelete={this.handelDelete}
+                  handelLike={this.handelLike}
+                />
+              </div>
+            ))}
+          </div>
+          <button type="submit" onClick={this.handlerButtonPrevious}>
+            {"\u003C"}
+          </button>
+          {currentNumberPage}
+          <button type="submit" onClick={this.handlerButtonNext}>
+            {"\u003E"}
+          </button>
+          <Select
+            name="nbFilmPerPage"
+            value={nbFilmPerPage}
+            onChange={this.handleChange}
+            options={options}
+          />
+          <Select
+            value={selectedCategories}
+            onChange={this.handleChangeCategory}
+            options={categoriesObject}
+            isMulti
+          />
+        </div>
+      );
+    }
+    return (
+      <div className="sweet-loading">
+        <ClipLoader
+          sizeUnit="px"
+          size={150}
+          color="#123abc"
+          loading={loading}
+        />
+      </div>
+    );
+  }
 }
 
 export default ListFilm;
