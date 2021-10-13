ef6723cbf5266bbae0231e9c8053f04e6716bc36
vanbaotran <vanbaotran201194@gmail.com>
2021-10-14 00:34:59 +0200

    adding filter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
-import MoviesList from './components/MoviesList'
-function App() {
+import React, {useState, useEffect} from 'react';
+import MoviesList from './components/MoviesList';
+import {movies$} from './movies';
 
+function App() {
+   const [categoryList, setCategoryList] = useState([])
+    const [filter, setFilter] = useState([]);
+    useEffect(() => {
+        movies$.then(res=>{
+            let categoryList = res.map(el => el.category)
+            let filteredList = categoryList.filter((el,id)=>categoryList.indexOf(el)===id)
+            setCategoryList(filteredList)
+        })
+    }, [])
+    const handleClick = (id) => {
+        setFilter([...filter,categoryList[id]])
+    }
+    const handleSubmit = () => {
+        
+    }
   return (
     <div className="App">
+      <div className='category-list'>
+        <form onSubmit={handleSubmit()}>
+            <label for="categories">Choose one or many categories:</label>
+                <select name={filter} id='options' multiple>
+                {categoryList.map((el,id)=>{
+                    return (
+                        <option value={el} onClick={()=>{handleClick(id)}}>{el}</option>
+                    )
+                })}
+                </select>
+            <input type='submit' value='search'/>
+        </form>
+        </div>
       <MoviesList />
     </div>
   );
