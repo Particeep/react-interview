2832599183cd3ff807114e310d7309caacedb884
Jehan-HENRY <jehan.henry@gmail.com>
2022-01-27 13:03:59 +0100

    add tailwaind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
