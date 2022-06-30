8e87d6d705cf1a34cb7404668fc8b9702fe385f6
mathildeL <liberta.github@gmail.com>
2022-06-30 20:46:20 +0200

    feat(movies): like/dislike

--- src/Movies/Movies.tsx
+++ src/Movies/Movies.tsx
@@ -1,25 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { movies$ } from "../movies";
+import React, {useEffect, useState} from "react";
+import {movies$} from "../movies";
 import _ from "lodash";
 import "./MoviesStyle.scss";
-import {bin, like, dislike} from "../svg";
+import {bin, dislike, like} from "../svg";
 
 interface Movie {
     id: number;
     title: string;
-    category:  string;
+    category: string;
     likes: number;
     dislikes: number;
 }
 
+interface LikedMovie {
+    id: number;
+    liked: string;
+}
+
+const VOTE = {LIKED: "LIKED", DISLIKED: "DISLIKED", NONE: "NONE"};
+
 export const Movies = () => {
-    const [init, setInit] = useState(false);
-    const [movies, setMovies] = useState(Array<Movie>);
+    const [init, setInit] = useState<boolean>(false);
+    const [movies, setMovies] = useState<Array<Movie>>([]);
+    const [liked, setLiked] = useState<Array<LikedMovie>>([]);
 
     useEffect(() => {
-        console.log("useEffect !! ");
-        if(!init) {
-            movies$.then((movies) => setMovies(movies));
+        console.log("RENDER : ", liked, movies)
+        if (!init) {
+            movies$.then((movies) => {
+                setMovies(movies);
+                setLiked(_.map(movies, (movie: Movie) => {
+                    return {id: movie.id, liked: VOTE.NONE}
+                }));
+            });
             setInit(true);
         }
     })
@@ -28,9 +41,46 @@ export const Movies = () => {
         return Math.ceil((value / totValues) * 100);
     }
 
-    function toggleDeleteBtn(id: number) {
+    function deleteBtn(id: number) {
         const newMovies = _.filter(movies, (movie: Movie) => !_.isEqual(movie.id, id));
         setMovies(newMovies);
+    }
+
+    function likingBtn(isLiked: boolean, id: number) {
+        const movieIndex = _.findIndex(movies, (movie: Movie) => _.isEqual(movie.id, id)!!);
+        let movie = _.find(movies, (movie: Movie) => _.isEqual(movie.id, id))!!;
+        const likes = movie!!.likes;
+        const dislikes = movie!!.dislikes;
+
+
+        const likedMovieIndex = _.findIndex(liked, (movie: LikedMovie) => _.isEqual(movie.id, id))!!;
+        let likedMovie = _.find(liked, (movie: LikedMovie) => _.isEqual(movie.id, id))!!;
+        console.log(likedMovie)
+        switch (likedMovie.liked) {
+            case VOTE.LIKED:
+                likedMovie.liked = isLiked ? VOTE.NONE : VOTE.DISLIKED;
+                movie.likes = likes - 1;
+                movie.dislikes = isLiked ? dislikes : dislikes + 1;
+                break;
+            case VOTE.DISLIKED:
+                likedMovie.liked = isLiked ? VOTE.LIKED : VOTE.NONE;
+                movie.likes = isLiked ? likes + 1 : likes;
+                movie.dislikes = dislikes - 1;
+                break;
+            case VOTE.NONE:
+                likedMovie.liked = isLiked ? VOTE.LIKED : VOTE.DISLIKED;
+                movie.likes = isLiked ? likes + 1 : likes;
+                movie.dislikes = !isLiked ? dislikes + 1 : dislikes;
+                break;
+        }
+
+        console.log(likedMovie, liked[likedMovieIndex], liked)
+        console.log(movie)
+        liked[likedMovieIndex] = likedMovie;
+        setLiked(liked);
+
+        movies[movieIndex] = movie;
+        setMovies(movies);
     }
 
     return (
@@ -39,20 +89,34 @@ export const Movies = () => {
                 {
                     movies && _.map(movies, (movie: Movie) => <div className="card-movie" key={movie.id}>
                         <div className="title">{movie.title}</div>
-                        <button className="bin-btn" onClick={() => toggleDeleteBtn(movie.id)}><img className="bin-icon" src={bin}/></button>
+                        <button className="bin-btn" onClick={() => deleteBtn(movie.id)}>
+                            <img className="bin-icon" src={bin}/>
+                        </button>
                         <div>{movie.category}</div>
-                        <button className="like-btn"><img className="like-icon" src={like}/>{movie.likes}</button>
-                        <button className="dislike-btn"><img className="dislike-icon" src={dislike}/>{movie.dislikes}</button>
+                        <button
+                            className={`like-btn`}
+                            onClick={() => likingBtn(true, movie.id)}>
+                            <img className="like-icon" src={like}/>
+                            {movie.likes}
+                        </button>
+                        <button className="dislike-btn" onClick={() => likingBtn(false, movie.id)}>
+                            <img className="dislike-icon" src={dislike}/>
+                            {movie.dislikes}
+                        </button>
                         <div className="liking-bar">
-                            <div className="like-bar" style={{width: `${getPercentage(movie.likes, (movie.likes + movie.dislikes))}%`}}></div>
+                            <div className="like-bar"
+                                 style={{width: `${getPercentage(movie.likes, (movie.likes + movie.dislikes))}%`}}></div>
                         </div>
                     </div>)
                 }
             </div>
             <footer>
-                <a href="https://www.flaticon.com/free-icons/bad" title="bad icons">Bad icons created by vectorspoint - Flaticon</a>
-                <a href="https://www.flaticon.com/free-icons/like" title="like icons">Like icons created by Pixel perfect - Flaticon</a>
-                <a href="https://www.flaticon.com/free-icons/trash" title="trash icons">Trash icons created by smalllikeart - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/bad" title="bad icons">Bad icons created by vectorspoint -
+                    Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/like" title="like icons">Like icons created by Pixel
+                    perfect - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/trash" title="trash icons">Trash icons created by
+                    smalllikeart - Flaticon</a>
             </footer>
         </div>
     );
