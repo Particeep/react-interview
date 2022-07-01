7d19ab712252a94dcba91c49748b4b517f4f15ea
mathildeL <liberta.github@gmail.com>
2022-07-01 08:08:29 +0200

    refactor(card): like/dislike

--- src/Movies/Movies.tsx
+++ src/Movies/Movies.tsx
@@ -2,9 +2,9 @@ import React, {useEffect, useState} from "react";
 import {movies$} from "../movies";
 import _ from "lodash";
 import "./MoviesStyle.scss";
-import {bin, dislike, like} from "../svg";
+import {MovieCard} from "../Card/MovieCard";
 
-interface Movie {
+export interface Movie {
     id: number;
     title: string;
     category: string;
@@ -17,97 +17,33 @@ interface LikedMovie {
     liked: string;
 }
 
-const VOTE = {LIKED: "LIKED", DISLIKED: "DISLIKED", NONE: "NONE"};
-
 export const Movies = () => {
     const [init, setInit] = useState<boolean>(false);
     const [movies, setMovies] = useState<Array<Movie>>([]);
-    const [liked, setLiked] = useState<Array<LikedMovie>>([]);
 
     useEffect(() => {
-        console.log("RENDER : ", liked, movies)
         if (!init) {
             movies$.then((movies) => {
                 setMovies(movies);
-                setLiked(_.map(movies, (movie: Movie) => {
-                    return {id: movie.id, liked: VOTE.NONE}
-                }));
             });
             setInit(true);
         }
     })
-
-    function getPercentage(value: number, totValues: number) {
-        return Math.ceil((value / totValues) * 100);
-    }
 
     function deleteBtn(id: number) {
         const newMovies = _.filter(movies, (movie: Movie) => !_.isEqual(movie.id, id));
         setMovies(newMovies);
     }
 
-    function likingBtn(isLiked: boolean, id: number) {
-        const movieIndex = _.findIndex(movies, (movie: Movie) => _.isEqual(movie.id, id)!!);
-        let movie = _.find(movies, (movie: Movie) => _.isEqual(movie.id, id))!!;
-        const likes = movie!!.likes;
-        const dislikes = movie!!.dislikes;
-
-
-        const likedMovieIndex = _.findIndex(liked, (movie: LikedMovie) => _.isEqual(movie.id, id))!!;
-        let likedMovie = _.find(liked, (movie: LikedMovie) => _.isEqual(movie.id, id))!!;
-        console.log(likedMovie)
-        switch (likedMovie.liked) {
-            case VOTE.LIKED:
-                likedMovie.liked = isLiked ? VOTE.NONE : VOTE.DISLIKED;
-                movie.likes = likes - 1;
-                movie.dislikes = isLiked ? dislikes : dislikes + 1;
-                break;
-            case VOTE.DISLIKED:
-                likedMovie.liked = isLiked ? VOTE.LIKED : VOTE.NONE;
-                movie.likes = isLiked ? likes + 1 : likes;
-                movie.dislikes = dislikes - 1;
-                break;
-            case VOTE.NONE:
-                likedMovie.liked = isLiked ? VOTE.LIKED : VOTE.DISLIKED;
-                movie.likes = isLiked ? likes + 1 : likes;
-                movie.dislikes = !isLiked ? dislikes + 1 : dislikes;
-                break;
-        }
-
-        console.log(likedMovie, liked[likedMovieIndex], liked)
-        console.log(movie)
-        liked[likedMovieIndex] = likedMovie;
-        setLiked(liked);
-
-        movies[movieIndex] = movie;
-        setMovies(movies);
-    }
-
     return (
         <div id="movies">
             <div className="cards-container">
                 {
-                    movies && _.map(movies, (movie: Movie) => <div className="card-movie" key={movie.id}>
-                        <div className="title">{movie.title}</div>
-                        <button className="bin-btn" onClick={() => deleteBtn(movie.id)}>
-                            <img className="bin-icon" src={bin}/>
-                        </button>
-                        <div>{movie.category}</div>
-                        <button
-                            className={`like-btn`}
-                            onClick={() => likingBtn(true, movie.id)}>
-                            <img className="like-icon" src={like}/>
-                            {movie.likes}
-                        </button>
-                        <button className="dislike-btn" onClick={() => likingBtn(false, movie.id)}>
-                            <img className="dislike-icon" src={dislike}/>
-                            {movie.dislikes}
-                        </button>
-                        <div className="liking-bar">
-                            <div className="like-bar"
-                                 style={{width: `${getPercentage(movie.likes, (movie.likes + movie.dislikes))}%`}}></div>
-                        </div>
-                    </div>)
+                    movies && _.map(movies, (movie: Movie) =>
+                        <MovieCard defaultMovie={movie} deleteCallback={(id: number) => deleteBtn(id)}
+                                   likingCallback={(id: number, likes: number, dislikes: number) => {
+                                   }}/>
+                    )
                 }
             </div>
             <footer>
