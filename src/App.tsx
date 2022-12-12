1467288f67914523caf6b3e008d510a5849e182e
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-12-13 00:08:47 +0100

    init react and ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span>Hello react</span>
+    </div>
+  );
+}
+
+export default App;
