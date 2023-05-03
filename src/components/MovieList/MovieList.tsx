ae11d4b4dab2e87d0c6ae1a3c50c0cffb43831b7
Steve Dias <stevediaspro@gmail.com>
2023-05-03 11:04:40 +0200

    refactor: redux

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import useListMovies from "../../hooks/useListMovies";
 import MovieCard from "../MovieCard/MovieCard";
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,12 @@
 import {
   configureStore,
-  MiddlewareArray,
-  PreloadedState,
 } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+
 
 import rootReducer from "./rootReducer";
 
 export let store = configureStore({
   reducer: rootReducer,
-  //middleware: new MiddlewareArray().concat(logger)
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
